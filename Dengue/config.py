a702f23936b1cd22e9737c37b31955d2f40a666f
MenegonMaza <andresmenegon.um@outlook.com>
2024-06-27 01:27:32 -0300

    Delete Dengue directory

--- Dengue/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/dengue_app'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
